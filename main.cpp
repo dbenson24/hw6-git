f052ed66e3eb1ad1341c8cde89646a6cdbef0838
dbenson24 <derek.benson@tufts.edu>
2014-12-09 12:30:57 -0500

    Adjusted main.cpp for commandline testing

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main(int argc, char *argv[])
 	std::string filename;
 	WordList wordlist;
 
-	/*if (argc == 2) { // if there is a filename on the command line
+	if (argc == 2) { // if there is a filename on the command line
 		wordlist.read_lyrics(argv[1],true);
 	}
-	else { // use a pre-defined filename*/
+	else { // use a pre-defined filename
 	filename = "/home/derek/Documents/text_dbs/rick_db.txt";
 	//filename = "/home/derek/Documents/text_dbs/count.txt";
 	//filename = "/home/derek/Documents/text_dbs/2316Songs.txt";
@@ -22,8 +22,8 @@ int main(int argc, char *argv[])
 	//filename = "/h/dbenso01/hw6/hw6-git/rick_db.txt";
 	//filename = "lyrics_fulldb.txt";
 	wordlist.read_lyrics(filename.c_str(),true);
-	//}
-	/*std::string query;
+	}
+	std::string query;
 	while (true){
 		std::cin >> query;
 		if (query == "<BREAK>")
@@ -33,8 +33,7 @@ int main(int argc, char *argv[])
 		else{
 			wordlist.search(query);
 		}
-	}*/
-	wordlist.search("cry");
+	}
 	std::cout << "<END OF REPORT>\n";
 	return 0;
 }
